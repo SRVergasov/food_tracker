89efab72f1c6ab0304e9b1ffd54392a3e486f640
Shamil <vergasov.shamil@yandex.com>
2025-01-31 20:02:35 +0300

    + models

--- web/models.py
+++ web/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Meal(models.Model):
+    meal_type = models.CharField(max_length=64)
+    date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class FoodItem(models.Model):
+    name = models.CharField(max_length=256)
+    calories = models.FloatField()
+    protein = models.FloatField()
+    fat = models.FloatField()
+    carbs = models.FloatField()
+
+
+class MealEntry(models.Model):
+    quantity = models.FloatField()
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+    food_item = models.ForeignKey(FoodItem, on_delete=models.CASCADE)
