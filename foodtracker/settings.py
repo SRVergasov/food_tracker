4d67a59c6f14615d83d5cfd3924e276061ce04b1
Shamil <vergasov.shamil@yandex.com>
2025-01-31 21:13:13 +0300

    + postgres

--- foodtracker/settings.py
+++ foodtracker/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'foodtracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'foodtracker',
+        'USER': 'foodtracker',
+        'PASSWORD': 'foodtracker',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
